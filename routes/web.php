4bcfdc5977b4146f5c76abfdb0ee1699145eaa02
Fernando Herrera <u.gomezherrera@gmail.com>
2024-09-15 16:55:51 -0600

    Auth Completed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,9 +19,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
+/*Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');*/
+
+Route::get('/home', [HomeController::class, 'index'])
+->middleware(['auth'])
+->name('home');
+
+/*Route::get('post', [HomeController::class, 'post'])->middleware(['auth','admin']) */
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
